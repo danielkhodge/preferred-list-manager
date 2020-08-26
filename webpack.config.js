2f1e2f84176b64d472c0996a8f5f7ee43464d170
Jason Hodge <jason.hodge@tabbank.com>
2020-08-26 16:16:01 -0600

    Removed all proxy code, changed startup back to normal with no proxy.

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-// Webpack Config
-devServer: {
-    headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': 'true',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization, x-id, X-Authorization, Content-Length, X-Requested-With',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'
-    }
- }
